5c5118a4930764544d1081707775390dce636742
Jared Menze <ja_menze@CIS.LSC.LOCAL>
2018-10-02 12:45:47 -0500

    Added in psuedo code while working through capitalization

--- Chapter 6/AuthoringAssistant.py
+++ Chapter 6/AuthoringAssistant.py
@@ -7,7 +7,6 @@ menu_options = ["c - Number of non-whitespace characters",
 whitespaces = [' ', '\t', '\n']
 punctuation = ['!', '.', ',', '?']
 user_option = ' '
-words = []
 
 def menu():
     global user_option
@@ -17,26 +16,51 @@ def menu():
     user_option = input("\nChoose an option: ")
 
 def count_nonws(user_text):
-    count = 0
+    count = 1
     for ch in  user_text:
         if ch not in whitespaces:
             count += 1
     print("Number of non-whitespace characters:", count)
 
 def count_words(user_text):
-    global words
-    count = 1
-    word = ''
-    for ch in user_text:
-        if ch in whitespaces:
-            words.append(word)
-            word = ''
-            count += 1
-        else:
-            word += ch
+    text = user_text.split()
+    count = len(text)
     print("Number of words:", count)
 
-menu_functions = {'c': count_nonws, 'w': count_words}
+def capitalize_words(user_text):
+    cap_count = 0
+    return_text = ''
+    edited = user_text.split()
+    #Capitalizing words that come after a period
+    edited[0] = edited[0].capitalize()
+    for i in range(len(edited)):
+        if edited[i][-1] == '.':
+            cap_count += 1
+            if i == len(edited) - 1:
+                edited[0] = edited[0].capitalize()
+            else:
+                edited[i + 1] = edited[i + 1].capitalize()
+    for x in range(len(edited)):
+        if x == len(edited):
+            return_text += edited[x]
+        else:
+            return_text += edited[x] + spacing[x]
+    print("Spacing:", spacing)
+    print("Words:", edited)
+    print("Edited text:", return_text)
+    #print("Number of letters capitalized:", cap_count)
+    
+def shorten_spaces(user_text):
+    return_text = ''
+    edited = user_text.split()
+    for word in edited:
+        if word == edited[-1]:
+            return_text += word
+        else:
+            return_text += word + ' '
+
+menu_functions = {'c': count_nonws, 'w': count_words, 'f': capitalize_words,
+                  's': shorten_spaces}
 
 if __name__ == '__main__':
     user_text = input("Enter a sample text:\n")
