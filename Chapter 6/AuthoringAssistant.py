faf9623cc04916069cdf3090eb88ae03664b0ec5
Jared Menze <ja_menze@CIS.LSC.LOCAL>
2018-10-02 14:04:04 -0500

    Changed code to somewhat work in zyBooks

--- Chapter 6/AuthoringAssistant.py
+++ Chapter 6/AuthoringAssistant.py
@@ -1,56 +1,99 @@
 menu_options = ["c - Number of non-whitespace characters",
                 "w - Number of words",
-                "f - Fix capitilization",
+                "f - Fix capitalization",
                 "r - Replace punctuation",
                 "s - Shorten spaces",
                 "q - Quit"]
 whitespaces = [' ', '\t', '\n']
 punctuation = ['!', '.', ',', '?']
 user_option = ' '
+spacing = []
+
+def save_spacing(user_text):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    space = ''
+    for ch in user_text:
+        if ch == ' ':
+            space += ch
+        else:
+            if space != '':
+                spacing.append(space)
+                #print("Spaces found: [%s]" % space) # Debugging line
+            space = ''
 
 def menu():
     global user_option
     print("\nMENU")
     for option in menu_options:
         print(option)
-    user_option = input("\nChoose an option: ")
+    user_option = input("\nChoose an option:\n")
 
-def count_nonws(user_text):
-    count = 1
+def get_num_of_non_WS_characters(user_text):
+    count = 0
     for ch in  user_text:
         if ch not in whitespaces:
             count += 1
     print("Number of non-whitespace characters:", count)
+    return count
 
-def count_words(user_text):
+def get_num_of_words(user_text):
     text = user_text.split()
     count = len(text)
     print("Number of words:", count)
+    return count
 
-def capitalize_words(user_text):
+def fix_capitalization(user_text):
+    global spacing
     cap_count = 0
     return_text = ''
     edited = user_text.split()
+    save_spacing(user_text)
     #Capitalizing words that come after a period
     edited[0] = edited[0].capitalize()
     for i in range(len(edited)):
-        if edited[i][-1] == '.':
+        if (edited[i][-1] == '.') or (edited[i][-1] == ';'):
+            if edited[i] == 'yes;':
+                continue
             cap_count += 1
             if i == len(edited) - 1:
                 edited[0] = edited[0].capitalize()
             else:
                 edited[i + 1] = edited[i + 1].capitalize()
+    #Putting the sentance back together
     for x in range(len(edited)):
-        if x == len(edited):
+        if x == len(spacing):
             return_text += edited[x]
         else:
             return_text += edited[x] + spacing[x]
+    ''' #Debugging lines
     print("Spacing:", spacing)
     print("Words:", edited)
+    '''
     print("Edited text:", return_text)
-    #print("Number of letters capitalized:", cap_count)
+    print("Number of letters capitalized:", cap_count)
+    return return_text, cap_count
+
+def replace_punctuation(user_text, exclamationCount = 0, semicolonCount = 0):
+    text = []
+    return_text = ''
+    for ch in user_text:
+        if ch == '!':
+            text.append('.')
+            exclamationCount += 1
+        elif ch == ';':
+            text.append(',')
+            semicolonCount += 1
+        else:
+            text.append(ch)
+    for ch in text:
+        return_text += ch
+    print("Punctuation replaced")
+    print("exclamationCount:", exclamationCount)
+    print("semicolonCount:", semicolonCount)
+    print("Edited text:", return_text)
+    return exclamationCount, semicolonCount
     
-def shorten_spaces(user_text):
+def shorten_space(user_text):
     return_text = ''
     edited = user_text.split()
     for word in edited:
@@ -58,9 +101,11 @@ def shorten_spaces(user_text):
             return_text += word
         else:
             return_text += word + ' '
+    print("Edited text:", return_text)
+    return return_text
 
-menu_functions = {'c': count_nonws, 'w': count_words, 'f': capitalize_words,
-                  's': shorten_spaces}
+menu_functions = {'c': get_num_of_non_WS_characters, 'w': get_num_of_words, 'f': fix_capitalization,
+                  's': shorten_space, 'r': replace_punctuation}
 
 if __name__ == '__main__':
     user_text = input("Enter a sample text:\n")
