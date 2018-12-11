6f2f83b1caa3133bf9b50e8057e4a88a7b95a32f
Jared Menze <ja_menze@CIS.LSC.LOCAL>
2018-12-11 14:16:38 -0600

    Finished up two-player code. Need to add menu and computer opponent

--- Final Project/Store/ShoppingCart.py
+++
@@ -1,9 +0,0 @@
-class Cart:
-    def __init__(self):
-        self.items = []
-        self.total_item_count = 0
-        self.total_cost = 0.0
-    
-    def count_total_items(self):
-        for item in self.items:
-            self.total_cost += item.price
--- GUI/Examples/TestGUI.py
+++ GUI/Examples/TestGUI.py
@@ -1,29 +1,32 @@
-from tkinter import *
+import tkinter
 
-class Application(Frame):
+class Application(tkinter.Frame):
     def say_hi(self):
         print ("hi there, everyone!")
 
     def createWidgets(self):
-        self.QUIT = Button(self)
+        self.QUIT = tkinter.Button(self)
         self.QUIT["text"] = "QUIT"
         self.QUIT["fg"]   = "red"
         self.QUIT["command"] =  self.quit
 
         self.QUIT.pack({"side": "left"})
 
-        self.hi_there = Button(self)
+        self.hi_var = tkinter.IntVar()
+
+        self.hi_there = tkinter.Button(self)
         self.hi_there["text"] = "Hello",
         self.hi_there["command"] = self.say_hi
+        self.hi_there["variable"] = self.hi_var
 
         self.hi_there.pack({"side": "left"})
 
     def __init__(self, master=None):
-        Frame.__init__(self, master)
+        tkinter.Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
 
-root = Tk()
+root = tkinter.Tk()
 app = Application(master=root)
 app.mainloop()
 root.destroy()
